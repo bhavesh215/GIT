0eafdd9b97656e1af4db59e4f01efb1b87679dbe
Bhavesh Uttawani <bhaveshuttawani@gmail.com>
2022-01-11 09:57:09 +0530

    Added diff.cpp in staging area and some changes are also made in sum.cpp

---
+++ diff.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int a=45;
+    int b=23;
+    cout<<"The difference of a and b is "<<a-b<<endl;
+    return 0;
+}
--- sum.cpp
+++ sum.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int a=12;
     int b=34;
-    cout<<"The sum of a and b is "<<a+b<<endl;
+    int c=0;
+    cout<<"The sum of a,b and c is "<<a+b+c<<endl;
     return 0;
 }
